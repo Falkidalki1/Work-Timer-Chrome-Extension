6c26c29671f7075f9c7ea66c6e5a05ebda17b018
Falkidalki1 <83883255+Falkidalki1@users.noreply.github.com>
2023-10-23 12:33:12 +0200

    Code cleanup
    Making the code a little more readable and renaming some files

--- breakscript.js
+++ breakscript.js
@@ -1,17 +1,12 @@
 var workTime;
 var breakTime;
 document.body.style.backgroundColor = "red"
-chrome.storage.local.get(["workDuration"], function(result) {
-  workTime = parseInt(result.workDuration);
-  console.log("Work time is currently " + workTime);
-  });
-
-  chrome.storage.local.get(["breakDuration"], function(result) {
-    breakTime = parseInt(result.breakDuration);
-    console.log("Break time is currently " + breakTime);
-    count()
-  });
-
+//Get the time set for the break timer
+chrome.storage.local.get(["breakDuration"], function (result) {
+  breakTime = parseInt(result.breakDuration);
+  console.log("Break time is currently " + breakTime);
+  count()
+});
 // Set the date we're counting down to
 function openTimerWindow() {
   chrome.windows.create({
@@ -20,34 +15,36 @@ function openTimerWindow() {
     type: 'popup',
     width: 300,
     height: 200,
-  }, function(newWindow) {
-    // Handle the new window if necessary
-  });}
-function count(){
-var countDownDate = new Date().getTime() + breakTime * 60 * 1000;
-console.log(breakTime + 3)
-// Update the count down every 1 second
-var x = setInterval(function() {
+  }, function (newWindow) {
+  });
+}
+
+// Set the date we're counting down to
+function count() {
+  var countDownDate = new Date().getTime() + breakTime * 60 * 1000;
+  console.log(breakTime + 3)
+  // Update the count down every 1 second
+  var x = setInterval(function () {
     // Get today's date and time
     var now = new Date().getTime();
-  
+
     // Find the distance between now and the count down date
     var distance = countDownDate - now;
-  
+
     // Time calculations for hours, minutes, and seconds
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
+
     // Format the numbers with leading zeros
     var formattedHours = hours.toString().padStart(2, '0');
     var formattedMinutes = minutes.toString().padStart(2, '0');
     var formattedSeconds = seconds.toString().padStart(2, '0');
-  
+
     // Display the result in the element with id="demo"
-    document.getElementById("demo").innerHTML =  formattedHours + ":"
-    + formattedMinutes + ":" + formattedSeconds;
-  
+    document.getElementById("demo").innerHTML = formattedHours + ":"
+      + formattedMinutes + ":" + formattedSeconds;
+
     // If the count down is finished, write some text
     if (distance < 0) {
       openTimerWindow();
